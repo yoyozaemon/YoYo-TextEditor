7853943162907663285fbfad47d243c550c260c3
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2022-08-02 12:41:56 +0530

    Update RawMode.cpp

---
+++ src/RawMode.cpp
@@ -0,0 +1,36 @@
+#include "RawMode.h"
+#include "Window.h"
+
+EditorConfig e;
+
+//Curses lib init
+
+void RawMode::init() 
+{
+    initscr();
+    raw();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    Window::update_screen();
+    signal(SIGWINCH, Window::handle_change);
+}
+
+void RawMode::disable()
+{
+    delwin(e.win);
+}
+
+void RawMode::enable()
+{
+    e.win = newwin(e.screenrows, e.screencols,e.cy,e.cx);
+}
+
+void RawMode::editor_set_status_message(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(e.statusmsg,sizeof(e.statusmsg),fmt,ap);
+    va_end(ap);
+    e.statusmsg_time = time(NULL);
+}
