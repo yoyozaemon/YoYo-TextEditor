105cc07b07fbbefcbe3098b7f89719f676ac4d89
yoyozaemon <operatingsystem6563@gmail.com>
2022-08-08 11:27:30 +0530

    Update Row.h

---
+++ src/Row.h
@@ -0,0 +1,25 @@
+#ifndef YOYO_SRC_ROW_H
+#define YOYO_SRC_ROW_H
+
+#include <cstdio>
+#include <cstring>
+
+#include "RawMode.h"
+#include "File.h"
+
+extern EditorConfig e;
+
+namespace Row {
+    int editor_row_rx_to_cx(erow *row, int rx);
+    int editor_row_cx_to_rx(erow *row, int cx);
+    void editor_row_insert_char(erow *row, int at, int c);
+    void update_row(erow *row);
+    //void editor_append_row(char *s, size_t len);
+    void editor_row_delete_char(erow *row, int at);
+    void free_row(erow *row);
+    void delete_row(int at);
+    void append_string(erow *row, char *s, size_t len);
+    void editor_insert_row(int at, char *s, size_t len);
+}
+
+#endif //YOYO_SRC_ROW_H
